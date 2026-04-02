cc5afb24f1c8ffa5ea5a51158d3aa179ce9bfc3f
YasinDoyle <yasindoyle97@gmail.com>
2026-04-02 14:17:28 +0800

    fix:修改welcomeContent

--- src/renderer/src/pages/ChatWelcome/WelcomeContent.tsx
+++ src/renderer/src/pages/ChatWelcome/WelcomeContent.tsx
@@ -14,26 +14,13 @@ export default function WelcomeContent() {
       <WelcomeRow>
         <p>
           {t(
-            '欢迎使用AiKnowledgeBase，这是一款简单好用的AI助手，支持知识库、模型API、分享、联网搜索、智能体，此软件开源免费，也期待您分享软件给别人来支持我们的发展。',
+            '欢迎使用AiKnowledgeBase，这是一款简单好用的AI助手，支持知识库、模型API、分享、联网搜索、智能体。',
           )}
         </p>
       </WelcomeRow>
+      <WelcomeRow>😀</WelcomeRow>
       <WelcomeRow>
-        <p>
-          {t(
-            'AiKnowledgeBase是一个新的AI项目，它还在努力快速成长中，如果您在使用过程中遇到什么问题，或者您对软件有什么功能需求，可以加入我们微信群给我们进行反馈。',
-          )}
-        </p>
-      </WelcomeRow>
-      <WelcomeRow>
-        <p>
-          {t('最后您可以给我们开源项目点星星，您的这点鼓励对我们继续迭代开源项目极其重要，感谢。')}
-        </p>
-        <p className="mt-20px">
-          <Button ghost onClick={jumpToGithub}>
-            ⭐ {t('去github点星星')}
-          </Button>
-        </p>
+        <p>{t('让我们开启一段新的对话吧')}🎯</p>
       </WelcomeRow>
       <WelcomeRow>
         <p>
