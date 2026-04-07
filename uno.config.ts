1e5b8a16942dc119a8086de296eec07377c03e64
YasinDoyle <yasindoyle97@gmail.com>
2026-04-07 16:06:20 +0800

    fix: 解决listitem过长问题，不全unocss规则

--- uno.config.ts
+++ uno.config.ts
@@ -27,10 +27,32 @@ export default defineConfig({
     [/^mr-(\d+)$/, (match) => ({ 'margin-right': `${match[1]}px` })],
     [/^mb-(\d+)$/, (match) => ({ 'margin-bottom': `${match[1]}px` })],
     [/^ml-(\d+)$/, (match) => ({ 'margin-left': `${match[1]}px` })],
+    [/^p-(\d+)$/, (match) => ({ padding: `${match[1]}px` })],
+    [
+      /^px-(\d+)$/,
+      (match) => ({ 'padding-left': `${match[1]}px`, 'padding-right': `${match[1]}px` }),
+    ],
+    [
+      /^py-(\d+)$/,
+      (match) => ({ 'padding-top': `${match[1]}px`, 'padding-bottom': `${match[1]}px` }),
+    ],
     [/^pt-(\d+)$/, (match) => ({ 'padding-top': `${match[1]}px` })],
     [/^pr-(\d+)$/, (match) => ({ 'padding-right': `${match[1]}px` })],
     [/^pb-(\d+)$/, (match) => ({ 'padding-bottom': `${match[1]}px` })],
     [/^pl-(\d+)$/, (match) => ({ 'padding-left': `${match[1]}px` })],
+    [/^m-(\d+)$/, (match) => ({ margin: `${match[1]}px` })],
+    [
+      /^mx-(\d+)$/,
+      (match) => ({ 'margin-left': `${match[1]}px`, 'margin-right': `${match[1]}px` }),
+    ],
+    [
+      /^my-(\d+)$/,
+      (match) => ({ 'margin-top': `${match[1]}px`, 'margin-bottom': `${match[1]}px` }),
+    ],
+    [/^gap-(\d+)$/, (match) => ({ gap: `${match[1]}px` })],
+    [/^text-(\d+)$/, (match) => ({ 'font-size': `${match[1]}px` })],
+    [/^max-h-(\d+)$/, (match) => ({ 'max-height': `${match[1]}px` })],
+    [/^max-w-(\d+)$/, (match) => ({ 'max-width': `${match[1]}px` })],
   ],
   // 自定义快捷方式
   shortcuts: {
